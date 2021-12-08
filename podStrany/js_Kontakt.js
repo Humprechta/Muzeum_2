4120da7bbb6ba3719a88f85d157e6bb4006da041
Humprechta <91117513+Humprechta@users.noreply.github.com>
2021-12-08 14:23:17 +0100

    Add files via upload

--- podStrany/js_Kontakt.js
+++ podStrany/js_Kontakt.js
@@ -1,4 +1,5 @@
-window.onload = function() {myFunction3()};	
+window.onload = function() {myFunction3(),myFunction2()};	
+window.onscroll = function() {myFunction()};
 
 function openNav01() {
   document.getElementById("mySidenav").style.width = "100%";/*velikost menu*/
@@ -7,6 +8,33 @@ function openNav01() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function myFunction2(selectTag) {
+  var listValue = selectTag.options[selectTag.selectedIndex].text;
+  document.getElementById("kontakt01").style.fontSize = listValue;
+  document.getElementById("otDoba").style.fontSize = listValue;
+  document.getElementById("vedeni").style.fontSize = listValue;
+  document.getElementById("vedeniNad").style.fontSize = listValue;
+  document.getElementById("sec").style.fontSize = listValue;
+  document.getElementById("oss01").style.fontSize = listValue;
+  document.getElementById("oss02").style.fontSize = listValue;
+  document.getElementById("oss03").style.fontSize = listValue;
+  document.getElementById("oss04").style.fontSize = listValue;
+}
+
 
 /*text k novinkam*/
 var i = 0;
